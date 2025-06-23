205bb3f59ac9a5ac2fbe8df5b52a0ef000f390ba
Macalo1998 <110059967+Macalo1998@users.noreply.github.com>
2025-06-23 11:57:44 +0000

    alcides abuson

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const pronoun = ['this', 'my', 'the', 'that'];
+  const adj = ['great', 'big','small','huge'];
+  const noun = ['jogger', 'racoon','murderer'];
+  const domain = ['.com','.net','.info','.org','.biz','.edu']
+  // for (const start of pronoun) {
+  //   for (const mid of adj) {
+  //     for (const end of noun) {
+  //       for (const dom of domain) {
+  //         result = start + mid + end + dom;
+  //         console.log(result);
+  //         result = ""
+  //       }
+  //     }
+  //   }
+  // }
+  const generateNames = pronoun.flatMap(pronoun =>
+  adj.flatMap(adj =>
+    noun.flatMap(noun =>
+      domain.map(domain => `${pronoun}${adj}${noun}${domain}`)
+    )
+  )
+);
+  console.log(generateNames);
 };
