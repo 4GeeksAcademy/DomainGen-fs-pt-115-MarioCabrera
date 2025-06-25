feca47c7c23999bb137b4d50c5e916ed9cbbadb5
Macalo1998 <110059967+Macalo1998@users.noreply.github.com>
2025-06-25 11:30:01 +0000

    DOMado

--- src/app.js
+++ src/app.js
@@ -1,33 +1,45 @@
-import "bootstrap";
-import "./style.css";
+import 'bootstrap';
+import '/style.css';
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+const GenerateList = () => {
   //write your code here
   const pronoun = ['this', 'my', 'the', 'that'];
-  const adj = ['great', 'big','small','huge'];
-  const noun = ['jogger', 'racoon','murderer'];
-  const domain = ['.com','.net','.info','.org','.biz','.edu']
-  // for (const start of pronoun) {
-  //   for (const mid of adj) {
-  //     for (const end of noun) {
-  //       for (const dom of domain) {
-  //         result = start + mid + end + dom;
-  //         console.log(result);
-  //         result = ""
-  //       }
-  //     }
-  //   }
-  // }
-  const generateNames = pronoun.flatMap(pronoun =>
-  adj.flatMap(adj =>
-    noun.flatMap(noun =>
-      domain.map(domain => `${pronoun}${adj}${noun}${domain}`)
+  const adj = ['great', 'big', 'small', 'huge'];
+  const noun = ['jogger', 'racoon', 'murderer'];
+  const domain = ['.com', '.net', '.info', '.org', '.biz', '.edu']
+  const list = pronoun.flatMap(pronoun =>
+    adj.flatMap(adj =>
+      noun.flatMap(noun =>
+        domain.map(domain => `${pronoun}${adj}${noun}${domain}`)
+      )
     )
   )
-);
-  console.log(generateNames);
+  return list
 };
+const SelectedList = (id) => {
+  const DomList = GenerateList();
+  const resultado = document.getElementById('list');
+  document.getElementById('list').innerHTML = ""
+  const filtrados = DomList.filter(item => item.includes(`.${id}`));
+  if (filtrados.length > 0) {
+    resultado.innerHTML = `
+      <h3>Dominios acabados en ${id}:</h3>
+      <ul class="overflow-y-scroll" id="listado">
+        ${filtrados.map(item => `<li>${item}</li>`).join('')}
+      </ul>
+    `;
+    document.getElementById("list").classList.add("border","border-secondary","rounded-3","border-3");
+  } else {
+    resultado.innerHTML = `<p>No se encontraron resultados para <strong>${id}</strong>.</p>`;
+  }
+}
+const Opciones = document.querySelectorAll('.btn');
+Opciones.forEach((opc) => {
+  opc.addEventListener("click", (event) => {
+    let id = event.target.id;
+    document.getElementById("list").style.display = "block"
+    SelectedList(id)
+  });
+});
+
+    document.getElementById("list").style.display = "none"
